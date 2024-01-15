32ba7c727337ec1afcc2a2ebcb92096094b2663d
Dion11235 <dipanbanik104@gmail.com>
2024-01-15 13:35:35 +0530

    some changes in setup and requirements - first commit

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import find_packages, setup
+from typing import List
 
-def get_dependencies(filepath):
+HYPHEN_E_DOT = "-e ."
+
+def get_dependencies(filepath:str)->List[str]: 
     with open(filepath, "r") as f:
         requirements = f.readlines()
-        requirements = [x.replace("/n","") for x in requirements]
+        requirements = [x.replace("\n","") for x in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
     return requirements
 
 
 setup(
     name="TELCO_churn_classification",
     author="Dipan Banik",
+    author_email="dipanthedataguy@gmail.com"
     version="0.0.1", # data.model.parameters
     packages=find_packages(),
     install_requires=get_dependencies("requirements.txt")
